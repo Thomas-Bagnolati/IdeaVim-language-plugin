d7cdb358d111b64f0f52d2c0939da45dacb85790
GitHub Action <action@github.com>
2023-05-19 00:30:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/thomasbagnolati/ideavimlanguageplugin/MyBundle.kt
+++ src/main/kotlin/com/github/thomasbagnolati/ideavimlanguageplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.thomasbagnolati.ideavimlanguageplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/thomasbagnolati/ideavimlanguageplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/thomasbagnolati/ideavimlanguageplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.thomasbagnolati.ideavimlanguageplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
